ab5b34e3b7182dd7c70b247584958a4511f99833
alanam79 <halo2382001@gmail.com>
2022-05-31 17:50:35 -0400

    finished questions

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const validator = require('validator');
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -22,140 +23,140 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 // const questions = () => {
-    const promptUser = () => {
+const promptUser = () => {
   return inquirer.prompt([
-  {
-    type: "input",
-    name: "github",
-    message: "Please enter your GitHub Username (Required)",
-    validate: (githubInput) => {
-      if (githubInput) {
-        return true;
-      } else {
-        console.log("Please enter your GitHub username!");
-        return false;
-      }
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter your GitHub Username (Required)",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
     },
-  },
-  {
-    type: "input",
-    name: "title",
-    message: "What is your project title? (Required)",
-    validate: (projectTitle) => {
-      if (projectTitle) {
-        return true;
-      } else {
-        console.log("Please enter your project title!");
-        return false;
-      }
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project title? (Required)",
+      validate: (projectTitle) => {
+        if (projectTitle) {
+          return true;
+        } else {
+          console.log("Please enter your project title!");
+          return false;
+        }
+      },
     },
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Please enter a short description of your project (Required)",
-    validate: (projectDescription) => {
-      if (projectDescription) {
-        return true;
-      } else {
-        console.log("Please enter your project description!");
-        return false;
-      }
+    {
+      type: "input",
+      name: "description",
+      message: "Please enter a short description of your project (Required)",
+      validate: (projectDescription) => {
+        if (projectDescription) {
+          return true;
+        } else {
+          console.log("Please enter your project description!");
+          return false;
+        }
+      },
     },
-  },
-  {
-    type: "checkbox",
-    name: "languages",
-    message: "What did you build this project with? (Check all that apply)",
-    choices: [
-      "Javascript",
-      "HTML",
-      "CSS",
-      "ES6",
-      "jQuery",
-      "Bootstrap",
-      "Node.js",
-    ],
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Please select a license for your project (Required):",
-    choices: [
-      "MIT",
-      "APACHE(2.0)",
-      "GPL(3.0)",
-      "BSD(3)",
-      "MPL(2.0)",
-      "CDDL(1.0)",
-      "EPL(2.0)",
-      "None",
-    ],
-    validate: (projectLicense) => {
-      if (projectLicense) {
-        return true;
-      } else {
-        console.log("Please choose a project license type!");
-        return false;
-      }
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (Check all that apply)",
+      choices: [
+        "Javascript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node.js",
+      ],
     },
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What command(s) should be run to install dependencies?",
-    default: "npm install",
-  },
-  {
-    type: "input",
-    name: "test",
-    message: "What command(s) should be run to run tests?",
-    default: "npm test",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "What does the user need to know about using the repo?",
-  },
-  {
-    type: "input",
-    name: "website",
-    message: "Please enter your project website (Required)",
-    validate: (projectWebsite) => {
-      if (projectWebsite) {
-        return true;
-      } else {
-        console.log("Please enter your project website!");
-        return false;
-      }
+    {
+      type: "list",
+      name: "license",
+      message: "Please select a license for your project (Required):",
+      choices: [
+        "MIT",
+        "APACHE(2.0)",
+        "GPL(3.0)",
+        "BSD(3)",
+        "MPL(2.0)",
+        "CDDL(1.0)",
+        "EPL(2.0)",
+        "None",
+      ],
+      validate: (projectLicense) => {
+        if (projectLicense) {
+          return true;
+        } else {
+          console.log("Please choose a project license type!");
+          return false;
+        }
+      },
     },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What's your email address? (Required)",
-    validate: (emailInput) => {
-      if (emailInput) {
-        return true;
-      } else {
-        console.log("Please enter your email!");
-        return false;
-      }
+    {
+      type: "input",
+      name: "installation",
+      message: "What command(s) should be run to install dependencies?",
+      default: "npm install",
     },
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message: "Please enter the contributor name(s) for this project:",
-    validate: (contributorName) => {
-      if (contributorName) {
-        return true;
-      } else {
-        console.log("Please enter contributor(s) name!");
-        return false;
-      }
+    {
+      type: "input",
+      name: "test",
+      message: "What command(s) should be run to run tests?",
+      default: "npm test",
     },
-  },
-]);
+    {
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using the repo?",
+    },
+    {
+      type: "input",
+      name: "website",
+      message: "Please enter your project website (Required)",
+      validate: (projectWebsite) => {
+        if (projectWebsite) {
+          return true;
+        } else {
+          console.log("Please enter your project website!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What's your email address? (Required)",
+      validate: (emailInput) => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("Please enter your email!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Please enter the contributor name(s) for this project:",
+      validate: (contributorName) => {
+        if (contributorName) {
+          return true;
+        } else {
+          console.log("Please enter contributor(s) name!");
+          return false;
+        }
+      },
+    },
+  ]);
 };
-promptUser().then(answers => console.log(answers));
+promptUser().then((answers) => console.log(answers));
